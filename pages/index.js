f2682587f007affeeb59575492c8504c20840692
cpro95 <cpro95@gmail.com>
2018-06-14 18:31:35 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -8,5 +8,6 @@ export default () => (
     <Layout>
         <h1>Main Page</h1>
         <img className="img-fluid" src="/static/background-image3.jpg" alt="background-image"/>
+        <button className="btn btn-primary" onClick={onClickHandler}>Hello</button>
     </Layout>
 )
