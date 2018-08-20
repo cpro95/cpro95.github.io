87070f5492232ae30115d278bfab7836e9ae8b17
Manki Kim <cpro95@gmail.com>
2018-08-20 20:58:29 +0900

    minor comment added in about page

--- pages/about.js
+++ pages/about.js
@@ -13,15 +13,20 @@ class About extends React.Component {
                 <div className="wraper d-flex flex-column justify-content-center align-items-center">
                     <div className="jumbotron w-75 text-white">
                         <h1 className="display-4">Hello, there!</h1>
-                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <p className="lead">
+                            I'm a novice for web dev. I made this site in my spare time.<br />
+                            There's lots of stuff for learn for web dev, like css, animation, etc.
+                         </p>
                         <hr className="my-4" />
-                            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                            <a className="btn btn-primary btn-lg" href="http://github.com/cpro95" role="button">Learn more</a>
+                        <p>This is a simple Bootstrap & NextJS app, if you want see the source code,<br />
+                            plese click below button
+                        </p>
+                        <a className="btn btn-primary btn-lg" href="http://github.com/cpro95" role="button">Learn more</a>
                     </div>
                 </div>
             </div>
-                );
-            }
-        }
-        
+        );
+    }
+}
+
 export default About;
