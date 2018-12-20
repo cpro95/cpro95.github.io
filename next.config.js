766d9c5e76e6c2c7664d31b56a3ac5f1d767cec7
cpro95 <cpro95@gmail.com>
2018-12-20 11:29:21 +0900

    works page with blog,markdown import

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+	webpack: (config) => {
+		config.module.rules.push(
+		  {
+			test: /\.md$/,
+			use: 'raw-loader'
+		  }
+		)
+	
+		return config
+	  },
 	exportPathMap: function() {
 		return {
 			'/': { page: '/' },
