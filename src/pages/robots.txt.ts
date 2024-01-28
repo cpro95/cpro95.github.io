dbd2f8754e9c470c17351bdaea4dc66e95d74555
cpro95 <cpro95@gmail.com>
2024-01-28 14:55:23 +0900

    robots.txt.ts file modified

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -8,6 +8,8 @@ Disallow: /nogooglebot/
 User-agent: *
 Allow: /
 
+#DaumWebMasterTool:28388e61878414e7ad0fba19ee4a23a13ade5d71e2589d54fe45f9ee847c459e:AMTsw6vTdZJxdXeFCU8NDQ==
+
 Sitemap: ${new URL("sitemap-index.xml", SITE.website).href}
 `.trim();
 
