d0503f12e6693bc3732dbfc126165d85f16616bb
cpro95 <cpro95@gmail.com>
2024-01-28 12:54:41 +0900

    astropapaer

--- pages/api/m-project.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Perform a redirection to the desired URL
-  res.writeHead(302, { Location: 'http://132.226.227.91:3000' });
-  res.end();
-}
