c48880e9b1091a77e2254f1b488b9b31c76eb120
cpro95 <cpro95@gmail.com>
2025-11-03 23:20:21 +0900

    fuse search

--- static/js/search.js
+++ static/js/search.js
@@ -1,116 +1,223 @@
-(() => {
-  fetch('/index.json')
-  .then(response => response.json())
-  .then(data => {
-    const fuse = new Fuse(data, {
-      keys: ['title','summary','tags'],
-      shouldSort: true,
-      includeMatches: true,
-      minMatchCharLength: 1, // 1글자부터 검색 가능하도록 수정
-      threshold: 0.4,        // 검색 정확도를 유연하게 수정 (핵심!)
-      ignoreLocation: true,
-    })
-  
-    document.getElementById('search-form').addEventListener('submit', function (e) {
-      e.preventDefault();
-      const data = new FormData(e.target)
-      const input = [...data.entries()][0][1]
-      const results = fuse.search(input)
-      displayResults(input, results)
-    });
-  });
-})();
+var fuse;
+var showButton = document.getElementById("search-button");
+var showButtonMobile = document.getElementById("search-button-mobile");
+var hideButton = document.getElementById("close-search-button");
+var wrapper = document.getElementById("search-wrapper");
+var modal = document.getElementById("search-modal");
+var input = document.getElementById("search-query");
+var output = document.getElementById("search-results");
+var first = output.firstChild;
+var last = output.lastChild;
+var searchVisible = false;
+var indexed = false;
+var hasResults = false;
 
-function displayResults(input, results) {
-  const searchResults = document.getElementById('search-result');
-  searchResults.setAttribute('style', 'display: block;')
-  searchResults.nextElementSibling.setAttribute('style', 'display: none;')
-  let html = renderResultsCountHtml(results.length, input)
-  if (results.length > 0) {
-    let li = renderResultsItemHtml(results)
-    html += `<ul>${li}</ul>`
-  }
-  searchResults.innerHTML = html
-}
+// Listen for events
+showButton ? showButton.addEventListener("click", displaySearch) : null;
+showButtonMobile
+  ? showButtonMobile.addEventListener("click", displaySearch)
+  : null;
+hideButton.addEventListener("click", hideSearch);
+wrapper.addEventListener("click", hideSearch);
+modal.addEventListener("click", function (event) {
+  event.stopPropagation();
+  event.stopImmediatePropagation();
+  return false;
+});
+document.addEventListener("keydown", function (event) {
+  // Forward slash to open search wrapper
+  if (event.key == "/") {
+    const active = document.activeElement;
+    const tag = active.tagName;
+    const isInputField =
+      tag === "INPUT" || tag === "TEXTAREA" || active.isContentEditable;
 
-function renderResultsCountHtml(count, input) {
-  let html = `
-<div class="TableObject border-gray-light py-3 mt-6">
-  <div class="user-repo-search-results-summary TableObject-item TableObject-item--primary v-align-top">
-    <strong>${count}</strong>
-      results
-      for "<strong>${input}</strong>"
-  </div>
-</div>
-`
-  return html
-}
-
-function renderResultsItemHtml(results) {
-  // modified from https://github.com/brunocechet/Fuse.js-with-highlight
-  var highlighter = function(resultItem){
-    resultItem.matches.forEach((matchItem) => {
-      var text = resultItem.item[matchItem.key];
-      var result = []
-      var matches = [].concat(matchItem.indices);
-      var pair = matches.shift()
-      
-      for (var i = 0; i < text.length; i++) {
-        var char = text.charAt(i)
-        if (pair && i == pair[0]) {
-          result.push('<span style="color: red;">')
-        }
-        result.push(char)
-        if (pair && i == pair[1]) {
-          result.push('</span>')
-          pair = matches.shift()
-        }
-      }
-      resultItem.highlight = result.join('');
-  
-      if(resultItem.children && resultItem.children.length > 0){
-        resultItem.children.forEach((child) => {
-          highlighter(child);
-        });
-      }
-    });
-  };  
-
-  let html = ``
-  results.forEach(result => {
-    highlighter(result)
-    // truncated highlight content
-    let truncated = result.highlight.substring(0, 2000)
-    // 한글(가-힣) 범위를 포함하도록 정규식 수정
-    const reg = /(<span style="color: red;">[a-zA-Z0-9\uAC00-\uD7A3]+<\/span>)/g
-    let array = truncated.split(reg)
-    // drop unstable part
-    array.pop()
-    let content = ""
-    if (array.length > 0) {
-      content = array.join('')
-    } else {
-      // fallback to no highlighted truncated content
-      content = result.item.content.substring(0, 2000)
+    if (!searchVisible && !isInputField) {
+      event.preventDefault();
+      displaySearch();
     }
-    html += `
-<li class="col-12 d-flex width-full py-4 border-top color-border-secondary public source">
-  <div class="col-12 d-inline-block">
-    <div class="d-inline-block mb-1">
-      <h3 class="wb-break-all">
-        <a href="${result.item.permalink}">${result.item.title}</a>
-      </h3>
-    </div>
+  }
 
-    <div>
-      <div class="col-12 d-inline-block text-gray mb-2 pr-4">
-        ${content} ...
-      </div>
-    </div>
+  // Esc to close search wrapper
+  if (event.key == "Escape") {
+    hideSearch();
+  }
 
-  </div>
-</li>
-`
-  })
-  return html
+  // Down arrow to move down results list
+  if (event.key == "ArrowDown") {
+    if (searchVisible && hasResults) {
+      event.preventDefault();
+      if (document.activeElement == input) {
+        first.focus();
+      } else if (document.activeElement == last) {
+        last.focus();
+      } else {
+        document.activeElement.parentElement.nextSibling.firstElementChild.focus();
+      }
+    }
+  }
+
+  // Up arrow to move up results list
+  if (event.key == "ArrowUp") {
+    if (searchVisible && hasResults) {
+      event.preventDefault();
+      if (document.activeElement == input) {
+        input.focus();
+      } else if (document.activeElement == first) {
+        input.focus();
+      } else {
+        document.activeElement.parentElement.previousSibling.firstElementChild.focus();
+      }
+    }
+  }
+
+  // Enter to get to results
+  if (event.key == "Enter") {
+    if (searchVisible && hasResults) {
+      event.preventDefault();
+      if (document.activeElement == input) {
+        first.focus();
+      } else {
+        document.activeElement.click();
+      }
+    }
+  }
+});
+
+// Update search on each keypress
+input.onkeyup = function (event) {
+  executeQuery(this.value);
+};
+
+function displaySearch() {
+  if (!indexed) {
+    buildIndex();
+  }
+  if (!searchVisible) {
+    document.body.style.overflow = "hidden";
+    wrapper.style.visibility = "visible";
+    input.focus();
+    searchVisible = true;
+  }
+}
+
+function hideSearch() {
+  if (searchVisible) {
+    document.body.style.overflow = "visible";
+    wrapper.style.visibility = "hidden";
+    input.value = "";
+    output.innerHTML = "";
+    document.activeElement.blur();
+    searchVisible = false;
+  }
+}
+
+function fetchJSON(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
+    }
+  };
+  httpRequest.open("GET", path);
+  httpRequest.send();
+}
+
+function buildIndex() {
+  var baseURL = wrapper.getAttribute("data-url");
+  baseURL = baseURL.replace(/\/?$/, "/");
+  fetchJSON(baseURL + "index.json", function (data) {
+    var options = {
+      shouldSort: true,
+      ignoreLocation: true,
+      threshold: 0.0,
+      includeMatches: true,
+      extendedSearch: true,
+      keys: [
+        { name: "title", weight: 0.8 },
+        { name: "section", weight: 0.2 },
+        { name: "summary", weight: 0.6 },
+      ],
+    };
+    /*var finalIndex = [];
+    for (var i in data) {
+      if(data[i].type != "users" && data[i].type != "tags" && data[i].type != "categories"){
+        finalIndex.push(data[i]);
+      }
+    }*/
+    fuse = new Fuse(data, options);
+    indexed = true;
+  });
+}
+
+function executeQuery(term) {
+  // 1. 검색어를 띄어쓰기 기준으로 나눕니다.
+  console.log(term);
+  const words = term.split(" ");
+
+  // 2. 각 단어 앞에 ' (작은따옴표)를 붙여 AND 검색 조건으로 만듭니다.
+  //    (단, 빈 문자열은 제외합니다.)
+  const and_query = words
+    .filter((word) => word.length > 0)
+    .map((word) => "'" + word)
+    .join(" ");
+
+  // let results = fuse.search(term);
+
+  // 3. 변환된 쿼리로 검색을 수행합니다.
+  let results = fuse.search(and_query);
+
+  // ... 이하 코드는 기존과 동일
+  let resultsHTML = "";
+
+  if (results.length > 0) {
+    results.forEach(function (value, key) {
+      var html = value.item.summary;
+      var div = document.createElement("div");
+      div.innerHTML = html;
+      value.item.summary = div.textContent || div.innerText || "";
+      var title = value.item.externalUrl
+        ? value.item.title +
+          '<span class="text-xs ml-2 align-center cursor-default text-neutral-400 dark:text-neutral-500">' +
+          value.item.externalUrl +
+          "</span>"
+        : value.item.title;
+      var linkconfig = value.item.externalUrl
+        ? 'target="_blank" rel="noopener" href="' + value.item.externalUrl + '"'
+        : 'href="' + value.item.permalink + '"';
+      resultsHTML =
+        resultsHTML +
+        `<li class="mb-2">
+          <a class="flex items-center px-3 py-2 rounded-md appearance-none bg-neutral-100 dark:bg-neutral-700 focus:bg-primary-100 hover:bg-primary-100 dark:hover:bg-primary-900 dark:focus:bg-primary-900 focus:outline-dotted focus:outline-transparent focus:outline-2" 
+          ${linkconfig} tabindex="0">
+            <div class="grow">
+              <div class="-mb-1 text-lg font-bold">
+                ${title}
+              </div>
+              <div class="text-sm text-neutral-500 dark:text-neutral-400">${
+                value.item.section
+              }<span class="px-2 text-primary-500">&middot;</span>${
+          value.item.date ? value.item.date : ""
+        }</span></div>
+              <div class="text-sm italic">${value.item.summary}</div>
+            </div>
+            <div class="ml-2 ltr:block rtl:hidden text-neutral-500">&rarr;</div>
+            <div class="mr-2 ltr:hidden rtl:block text-neutral-500">&larr;</div>
+          </a>
+        </li>`;
+    });
+    hasResults = true;
+  } else {
+    resultsHTML = "";
+    hasResults = false;
+  }
+
+  output.innerHTML = resultsHTML;
+  if (results.length > 0) {
+    first = output.firstChild.firstElementChild;
+    last = output.lastChild.firstElementChild;
+  }
 }
